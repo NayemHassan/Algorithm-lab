864eaef168091ef46c12fffa71b2c2614f835484
Md.Nayem Hassan <66296692+nayem12498@users.noreply.github.com>
2020-06-16 12:54:26 +0600

    Update GCD.c

--- GCD.c
+++ GCD.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-/* Function declaration */
 int gcd(int a, int b);
 int main()
 {
@@ -10,10 +9,7 @@ int main()
     hcf = gcd(num1, num2);
     printf("GCD of %d and %d = %d", num1, num2, hcf);
     return 0;
-}
-/**
- * Recursive approach of euclidean algorithm to find GCD of two numbers
- */
+
 int gcd(int a, int b)
 {
     if(b == 0)
